6dcecb15c42dfb932d2619fa0f9d8c94250981a2
JimNikou <119452959+JimNikou@users.noreply.github.com>
2023-03-17 01:08:19 +0200

    Revert "WORKING MAIN!!!!"
    This reverts commit e7dc6a56f85e5f911608b7947811aa8616b89943.

--- main.cpp
+++ main.cpp
@@ -48,14 +48,8 @@
 
 
 namespace myNamespace{
-    float fin_ans,sec_ans;
+    float fin_ans,sec_ans=10;
     float ok;
-    std::vector<QString> passwords;
-    std::vector<QString> emails;
-}
-
-namespace username_change{
-    extern std::vector<std::string> full_name;
 }
 
 int main(int argc, char *argv[])
@@ -65,90 +59,28 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv); //creates the application
 
 
-
     login_sign_up_in loginWindow;
     loginWindow.setWindowModality(Qt::ApplicationModal);
-    QNetworkAccessManager manager;
-        QNetworkRequest request(QUrl("https://realtimeqttest-default-rtdb.europe-west1.firebasedatabase.app/Emails.json"));
+    loginWindow.show();
 
-        QNetworkReply *reply = manager.get(request);
+    // Run the application event loop until the login/sign up window is closed
+    int result = app.exec();
 
-        QObject::connect(reply, &QNetworkReply::finished, [&]() {
-            QByteArray data = reply->readAll();
-            QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
-                QJsonObject jsonObject = jsonDoc.object();
-
-
-                for (auto iter = jsonObject.constBegin(); iter != jsonObject.constEnd(); ++iter) {
-                    QString key = iter.key();
-                    QString value = iter.value().toString();
-
-                    if (value.contains("@")) {
-                        myNamespace::emails.push_back(value);
-                    }
-                }
-
-                // print the emails in the vector
-                for (const auto& email : myNamespace::emails) {
-                    std::cout << email.toStdString() << std::endl;
-                }
-            reply->deleteLater();
-        });
-
-
-        QNetworkAccessManager manager1;
-            QNetworkRequest request1(QUrl("https://realtimeqttest-default-rtdb.europe-west1.firebasedatabase.app/Passwords.json"));
-
-            QNetworkReply *reply1 = manager1.get(request1);
-
-            QObject::connect(reply1, &QNetworkReply::finished, [&]() {
-                QByteArray data1 = reply1->readAll();
-//                QString qans = QString::fromUtf8(data1);
-//                       std::string c_ans = qans.toStdString();
-//                       qDebug() << "JSON data:" << qans;
-                QJsonDocument jsonDoc1 = QJsonDocument::fromJson(data1);
-                    QJsonObject jsonObject1 = jsonDoc1.object();
+    // If the login/sign up window was accepted, show the main window
+    if (result == QDialog::Accepted) {
+        MainWindow mainWindow;
+        mainWindow.show();
+        return app.exec();
+    } else {
+        return 0;
+    }
 
 
 
-                    for (auto iter1 = jsonObject1.constBegin(); iter1 != jsonObject1.constEnd(); ++iter1) {
-                        QString key1 = iter1.key();
-                        QString value1 = iter1.value().toString();
-                        myNamespace::passwords.push_back(value1);
-                    }
-
-                    // print the emails in the vector
-                    for (const auto& passwords : myNamespace::passwords) {
-                        std::cout << passwords.toStdString() << std::endl;
-                    }
-                reply1->deleteLater();
-            });
-
-
-            QNetworkAccessManager manager2;
-                QNetworkRequest request2(QUrl("https://realtimeqttest-default-rtdb.europe-west1.firebasedatabase.app/FullName.json"));
-
-                QNetworkReply *reply2 = manager2.get(request2);
-
-                QObject::connect(reply2, &QNetworkReply::finished, [&]() {
-                    QByteArray data2 = reply2->readAll();
-    //                QString qans = QString::fromUtf8(data1);
-    //                       std::string c_ans = qans.toStdString();
-    //                       qDebug() << "JSON data:" << qans;
-                    QJsonDocument jsonDoc2 = QJsonDocument::fromJson(data2);
-                        QJsonObject jsonObject2 = jsonDoc2.object();
 
 
 
-                        for (auto iter2 = jsonObject2.constBegin(); iter2 != jsonObject2.constEnd(); ++iter2) {
-                            QString key2 = iter2.key();
-                            QString value2 = iter2.value().toString();
-                            std::string value_last = value2.toStdString();
-                            username_change::full_name.push_back(value_last);
-                        }
 
-                    reply2->deleteLater();
-                });
 
 
     //start of real time reading from firebase
@@ -164,49 +96,62 @@ int main(int argc, char *argv[])
     QTimer timer;
     timer.start(100); // send a request every 100 milli-seconds
 
-    // put timer and manager to same thread
-    realtime_manager.moveToThread(&thread);
-    timer.moveToThread(&thread);
+////start of real time reading from firebase
+//        QNetworkAccessManager manager;
+
+//        // creating a QThread to run the timer on
+//        QThread thread;
+//        // setting up the request
+//        QNetworkRequest request;
+//        request.setUrl(QUrl("https://realtimeqttest-default-rtdb.europe-west1.firebasedatabase.app/Dog.json"));
+
+//        // creating a QTimer to send requests at regular intervals
+//        QTimer timer;
+//        timer.start(100); // send a request every 100 milli-seconds
+
+//        // put timer and manager to same thread
+//        manager.moveToThread(&thread);
+//        timer.moveToThread(&thread);
 
 
 
-    // connect the timeout signal of the timer to a slot that sends the request and processes the response
-    QObject::connect(&timer, &QTimer::timeout, [&]() {
-        // sending the request and waiting for the response
-        QNetworkReply *reply = realtime_manager.get(request);
-        // creating an event loop to process the response
-        QEventLoop realtime_loop;
-        QAbstractSocket::connect(reply, &QNetworkReply::finished, &realtime_loop, &QEventLoop::quit); //if there is data it then stops the connection
-        realtime_loop.exec();
+//        // connect the timeout signal of the timer to a slot that sends the request and processes the response
+//        QObject::connect(&timer, &QTimer::timeout, [&]() {
+//            // sending the request and waiting for the response
+//            QNetworkReply *reply = manager.get(request);
 
-        // printf the response data
-        QString qans = reply->readAll(); //convert to QString
-        std::string c_ans = qans.toStdString(); //convert to regural C++ string
+//            // creating an event loop to process the response
+//            QEventLoop loop;
+//            QAbstractSocket::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit); //if there is data it then stops the connection
+//            loop.exec();
 
-        //removing '"' and holding on to '.' for it to be converted to float
-        c_ans.erase(std::remove_if(c_ans.begin(), c_ans.end(), [](char c){return !std::isdigit(c) && c != '.' && c != ' ';}), c_ans.end());
+//            // printf the response data
+//            QString qans = reply->readAll(); //convert to QString
+//            std::string c_ans = qans.toStdString(); //convert to regural C++ string
 
-        std::string first,second; //defining the first and the second numbers
+//            //removing '"' and holding on to '.' for it to be converted to float
+//            c_ans.erase(std::remove_if(c_ans.begin(), c_ans.end(), [](char c){return !std::isdigit(c) && c != '.' && c != ' ';}), c_ans.end());
 
-        std::stringstream ss(c_ans);
-        ss >> first >> second; //spliting the string on space
+//            std::string first,second; //defining the first and the second numbers
+
+//            std::stringstream ss(c_ans);
+//            ss >> first >> second; //spliting the string on space
 
 
 
 
-        myNamespace::fin_ans = atof(first.c_str()); //convert to float
-        myNamespace::sec_ans = atof(second.c_str()); //also
-        std::cout << std::setprecision(12) << myNamespace::fin_ans << "  |||  " << std::setprecision(8) << myNamespace::sec_ans << std::endl; //print output
-//        with float precision (counting the decimal numbers also) 12.
+//            myNamespace::fin_ans = atof(first.c_str()); //convert to float
+//            myNamespace::sec_ans = atof(second.c_str()); //also
+//            std::cout << std::setprecision(12) << myNamespace::fin_ans << "  |||  " << std::setprecision(8) << myNamespace::sec_ans << std::endl; //print output
+//    //        with float precision (counting the decimal numbers also) 12.
 
     });
 
-    //start the thread
-    thread.start();
-    ////end of reading from firebase
-    loginWindow.show();
+//    //start the thread
+//    thread.start();
+////end of reading from firebase
 
-    return app.exec();
+
 }
 
 
